d4f44970040979e2932fb97683057c4965f40c6d
Wojciech <wenter2005pl@gmail.com>
2025-05-11 15:36:46 +0200

    com1

---
+++ src/GamePanel.java
@@ -0,0 +1,25 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+
+public class GamePanel extends JPanel {
+    //SETTINGS
+    final int originalTileSize = 32;
+    //32x32
+    final int scale =2;
+    final int titleSize = originalTileSize*scale; //64x64
+    final int maxScreenCol = 16;
+    final int maxScreenRow=12;
+    final int screenWidth = titleSize * maxScreenCol;//1024 pix
+    final int screenHeight = titleSize * maxScreenRow;//768 pix
+
+
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("DziadiMysz");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationByPlatform(null);
+        window.setVisible(true);
     }
     //pushshe
 }
